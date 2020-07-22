3431aed921b09fe711d9b1c890fa9431b9df9a37
YoungChanShin <chan8149@naver.com>
2020-07-23 08:54:40 +0900

    update main css

--- frontend/src/components/HashTag.vue
+++ frontend/src/components/HashTag.vue
@@ -6,33 +6,44 @@
 </template>
 
 <script>
-  export default {
-    name: "HashTag",
-    data() {
-      return {
-        stacks: ['python','java','javascript','c','c++','frontend','backend','vue','spring']
-      }
-    }
-  }
+export default {
+  name: "HashTag",
+  data() {
+    return {
+      stacks: [
+        "python",
+        "java",
+        "javascript",
+        "c",
+        "c++",
+        "frontend",
+        "backend",
+        "vue",
+        "spring",
+      ],
+    };
+  },
+};
 </script>
 
 <style scoped>
-  .hashtag {
-    border-radius: 4px;
-    background-color: white;
-    margin: 3rem;
-    position: fixed;
-    right: 0;
-    padding: 10px;
-  }
-  .stack {
-    font-size: 1rem;
-    margin-bottom: 1rem;
-    border-radius: 6rem;
-    color: white;
-    font-weight: bolder;
-    background-color: #2c3e50;
-    padding: 3px;
-    z-index: -1;
-  }
+.hashtag {
+  border-radius: 4px;
+  background-color: white;
+  /* margin: 3rem; */
+  /* position: fixed; */
+  /* right: 0; */
+  /* padding: 10px; */
+  /* top: 100px; */
+}
+.stack {
+  font-size: 1rem;
+  margin-bottom: 1rem;
+  border-radius: 6rem;
+  color: white;
+  font-weight: bolder;
+  background-color: #2c3e50;
+  padding: 3px;
+  z-index: -1;
+}
 </style>
