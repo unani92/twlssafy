64f7381ea3102ce26f1250ba8ba6614912d3b807
배유진 <seow0124@naver.com>
2020-07-23 14:59:29 +0900

    BE / PinLikesFollow , FE / article card keywords type 수정

---
+++ backend/src/main/java/com/web/curation/model/pinlikesfollow/LikesMultiId.java
@@ -0,0 +1,10 @@
+package com.web.curation.model.pinlikesfollow;
+
+import java.io.Serializable;
+
+public class LikesMultiId implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    String email;
+    int articleid;
+}
