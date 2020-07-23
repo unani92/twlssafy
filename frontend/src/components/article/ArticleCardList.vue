57247aa217b999ea760c93095b847d2640e8d3e4
YoungChanShin <chan8149@naver.com>
2020-07-23 16:35:42 +0900

    Fix HashTag Design

--- frontend/src/components/article/ArticleCardList.vue
+++ frontend/src/components/article/ArticleCardList.vue
@@ -1,32 +1,83 @@
 <template>
   <div class="article-list-body">
     <ArticleCard
-      v-for="(article,index) in articles"
+      v-for="(article, index) in articles"
       :key="article._id"
       :article="article"
       :keywords="keywords[index]"
     ></ArticleCard>
+    <div id="bottomSensor"></div>
   </div>
 </template>
 
 <script>
 import ArticleCard from "@/components/article/AricleCard.vue";
+import { fetchArticles } from "@/api/index";
+import scrollMonitor from "scrollmonitor";
 
 export default {
   components: {
     ArticleCard,
   },
-  props: {
-    articles: {
-      type: Array,
-      required: true,
+
+  data() {
+    return {
+      articles: [],
+      keywords: [],
+      isLoading: true,
+      page: 0,
+    };
+  },
+  methods: {
+    async fetchData() {
+      console.log("ACTION!!!!!!!!!!!!");
+      const params = {
+        page: this.page++,
+      };
+      const { data } = await fetchArticles(params);
+      this.isLoading = false;
+      this.keywords = data.object.keyword;
+      this.articles = [...this.articles, ...data.object.article.content];
     },
-    keywords : {
-      type : Array,
-      required: true,
-    }
+    addScrollWatcher() {
+      const bottomSensor = document.querySelector("#bottomSensor");
+      console.log(bottomSensor);
+      const watcher = scrollMonitor.create(bottomSensor);
+      watcher.enterViewport(() => {
+        this.fetchData();
+      });
+    },
+  },
+  created() {
+    this.fetchData();
+  },
+  mounted() {
+    setTimeout(() => this.addScrollWatcher(), 1000);
   },
 };
 </script>
 
-<style></style>
+<style>
+#bottomSensor {
+  height: 10px;
+}
+
+::-webkit-scrollbar {
+  -webkit-appearance: none;
+}
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+::-webkit-scrollbar-track {
+  background: #eee;
+  border: 1px solid #eee;
+}
+
+::-webkit-scrollbar-thumb {
+  background: rgb(144, 153, 240);
+  border: 1px solid #eee;
+  height: 100px;
+  border-radius: 5px;
+}
+</style>
