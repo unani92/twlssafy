9e5483f8c936066660e7dc6a40286b03bfbfe2af
rain151112@naver.com <rain151112@naver.com>
2020-07-27 18:23:07 +0900

    BE / add keyword, likes, pin & change post -> getmapping

--- backend/src/main/java/com/web/curation/controller/SearchController.java
+++ backend/src/main/java/com/web/curation/controller/SearchController.java
@@ -9,9 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.data.domain.Page;
@@ -20,12 +19,18 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.web.curation.dao.KeywordsDao;
 import com.web.curation.dao.SearchDao;
+import com.web.curation.dao.SkillsDao;
+import com.web.curation.dao.pinlikesfollow.LikesDao;
+import com.web.curation.dao.pinlikesfollow.PinDao;
 import com.web.curation.model.Article;
 import com.web.curation.model.BasicResponse;
+import com.web.curation.model.Keywords;
 import com.web.curation.model.Search;
 
 @Api("Search Controller")
@@ -40,14 +45,28 @@ public class SearchController {
     @Autowired
     SearchDao searchDao;
 
+    @Autowired
+    SkillsDao skillsDao; 
+
+    @Autowired
+    KeywordsDao keywordsDao;
+    
+    @Autowired
+    LikesDao likesDao;
+
+    @Autowired
+    PinDao pinDao;
 
     @ApiOperation(value = "글 검색")
-        @PostMapping("/article/search")
+    @GetMapping("/article/search")
+    // @GetMapping("/article/search?q={q}&category={category}&page={page}")
     public Object searchArticle(@RequestParam String q, @RequestParam String category, @RequestParam int page) {
         final BasicResponse result = new BasicResponse();
 
         result.status = false;
         result.data = "글 검색 실패";
+
+        // int page2 = Integer.parseInt(page);
 
         // 글 검색
         Specification<Article> s = null;
@@ -65,15 +84,40 @@ public class SearchController {
                 return new ResponseEntity<>(result, HttpStatus.OK);
         }
 
-
-        // Specification<Article> s = Search.searchByTitle(search).or(Search.searchByWriter(search))
-        //     .or(Search.searchByContent(search));
         Page<Article> articles = searchDao.findAll(s, PageRequest.of(page, 10, Sort.Direction.DESC,"articleid"));
        
+        // 키워드 받아오기
+        List<List<String>> keywordsList = new ArrayList<>();
+        List<Integer> likesList = new ArrayList<>();
+        List<Integer> pinList = new ArrayList<>();
+
+        for(Article a : articles){
+            List<Keywords> tmpKeyword = keywordsDao.findAllByArticleid(a.getArticleid());
+            if(tmpKeyword!=null){ // 임시리스트 만들어서 키워드들 넣고, 최종 리스트에 담음
+                List<String> tmplist = new ArrayList<>();                   
+                 for(Keywords k : tmpKeyword){
+                        tmplist.add(skillsDao.findSkillBySno(k.getSno()).getName());
+                    }
+                keywordsList.add(tmplist);
+            }
+            else {
+                result.data="keyword 조회 실패";                    
+                return new ResponseEntity<>(result, HttpStatus.OK); // 글에 keyword 없으면 false return
+            }
+            likesList.add(likesDao.countByArticleid(a.getArticleid()));
+            pinList.add(pinDao.countByArticleid(a.getArticleid()));
+        }
+
+        Map<String,Object> object = new HashMap<>();
+        object.put("article",articles);
+        object.put("keyword", keywordsList);
+        object.put("likesCntList", likesList);
+        object.put("pinCntList", pinList);
+
        if(!articles.isEmpty()){
            result.status = true;
            result.data = "success";
-           result.object = articles;
+           result.object = object;
        }
 
        
