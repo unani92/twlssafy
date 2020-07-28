91d8d4e684198d05c9f5e16c9ffe2af851763bb0
rain151112@naver.com <rain151112@naver.com>
2020-07-28 17:52:59 +0900

    BE / socialmemberdao

--- backend/src/main/java/com/web/curation/dao/SocialMemberDao.java
+++ backend/src/main/java/com/web/curation/dao/SocialMemberDao.java
@@ -2,11 +2,14 @@ package com.web.curation.dao;
 
 import java.util.Optional;
 import com.web.curation.model.user.SocialMember;
+import com.web.curation.model.user.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SocialMemberDao extends JpaRepository<SocialMember, String> {
 
     Optional<SocialMember> findSocialmemberByEmail (String email);
+
+	Optional<User> findUserByEmail(String followemail);
 
 }
