04b5c9e0ca3b36cdc3c15ee76a4e91436eff56a9
배유진 <seow0124@naver.com>
2020-07-30 16:21:52 +0900

    BE / following 글 보기 pin 글보기

--- backend/src/main/java/com/web/curation/controller/ArticleController.java
+++ backend/src/main/java/com/web/curation/controller/ArticleController.java
@@ -307,6 +307,45 @@ public class ArticleController {
         return new ResponseEntity<>(result, HttpStatus.OK);
 
     }
+
+
     
+    @ApiOperation(value = "팔로잉 글 조회")
+    @GetMapping("/article/following")
+    public Object getFollowingArticle(@RequestParam(value = "page") final int page, @RequestHeader final HttpHeaders header) throws Exception {
+
+        final BasicResponse result = new BasicResponse();
+        result.status = false;
+        result.data = "fail";
+
+        String id_token = header.get("id_token").get(0);
+        String email = JWTDecoding.decode(id_token);
+
+        List<Article> list = articleDao.articleFromFollowing(email);
+
+        if(list!=null){
+            result.status = true;
+            result.data = "success";
+
+            int totalArticle = list.size();
+            // int totalPage = totalArticle/10;
+            // if (totalArticle%10 > 0) 
+            //     totalPage += 1;
+            
+            List<Article> articles = new ArrayList<>();
+
+            for(int i=page*10; i<page*10+10; i++) {
+                if(i<totalArticle) {
+                    articles.add(list.get(i));
+                }
+            }
+
+            result.object = articles;
+        }
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+  
 }
 
