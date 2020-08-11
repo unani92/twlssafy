85425e4178a4aec5390d48cce0c69370b3d9c3a3
배유진 <seow0124@naver.com>
2020-08-11 14:02:56 +0900

    Delete Account

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -22,8 +22,8 @@ export default {
             if (confirm("탈퇴 하시겠습니까?")) {
                 delAccount(this.$store.state.id_token).then((res) => {
                     if(res.data.status) {
-                        alert("탈퇴 되었습니다!");
                         this.$router.push({ name: "Logout" });
+                        alert("탈퇴 되었습니다!");
                     } else {
                         alert ("회원탈퇴 실패 ! 관리자에게 문의하세요.")
                     }
