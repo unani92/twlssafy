9712ab39e4c2b200dd485db1a17f5ccf84209cd5
unani <unani92@naver.com>
2020-08-13 11:49:51 +0900

    detailpage css v1

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -43,6 +43,7 @@
   }
 </script>
 <style>
+  @import url('https://fonts.googleapis.com/css2?family=Nanum+Gothic&family=Nanum+Gothic+Coding&family=Noto+Sans+KR:wght@300&display=swap');
   #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
@@ -61,34 +62,53 @@
     border-left-width: 4px !important;
     border-left-style: solid !important;
     border-left-color: #e6837a !important;
-    background-color: #d5dbd9;
+    background-color: #eeeeee;
   }
   blockquote > p {
     color: #0d0925 !important;
     padding: 10px;
   }
-  .tui-editor-contents {
+  .tui-editor-contents p {
     font-size: 1rem !important;
     font-family: 'Nanum Gothic Coding', monospace !important;
+    /*font-family: 'Nanum Gothic', sans-serif;*/
   }
   .tui-editor-contents > p {
     font-size: 1rem !important;
     word-break: break-all !important;
   }
+  .tui-editor-contents pre {
+    margin: 2px 0 8px;
+    padding: 18px;
+    background-color: #f5f7f8;
+    font-size: 1rem;
+  }
   .tui-editor-contents > h1 {
     font-size: 48px !important;
+    padding: 2rem 0 2rem 0 !important;
+    margin: 1rem 0 1rem 0 !important;
+    font-family: 'Nanum Gothic', sans-serif;
   }
   .tui-editor-contents > h2 {
     font-size: 40px !important;
+    padding: 2rem 0 2rem 0 !important;
+    margin: 1rem 0 1rem 0 !important;
+    font-family: 'Nanum Gothic', sans-serif;
   }
   .tui-editor-contents > h3 {
     font-size: 32px !important;
+    padding: 1rem 0 1rem 0 !important;
+    font-family: 'Nanum Gothic', sans-serif;
   }
   .tui-editor-contents > h4 {
     font-size: 28px !important;
+    padding: 1rem 0 1rem 0 !important;
+    font-family: 'Nanum Gothic', sans-serif;
   }
   .tui-editor-contents > h5 {
     font-size: 16px !important;
+    padding: 1rem 0 1rem 0 !important;
+    font-family: 'Nanum Gothic', sans-serif;
   }
   blockquote {
     font-size: 1rem;
