8a4a4292118edf78e32a426f2f167ed968930e2e
YoungChanShin <chan8149@naver.com>
2020-08-14 19:13:14 +0900

    navbar

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -85,7 +85,7 @@
     <div class="userMenu disabled">
       <div class="userMenu-menu">
         <div @click="goToMyPage" class="mypage-text">MyPage</div>
-        <div v-if="isGoogleUser" @click="goToPwChange" class="mypage-text">비밀번호 변경</div>
+        <div v-if="!isGoogleUser" @click="goToPwChange" class="mypage-text">비밀번호 변경</div>
         <div @click="logout" class="mypage-text">Log Out</div>
       </div>
     </div>
