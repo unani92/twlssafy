c553a65c8e8557a7f638b88d972e44b8f85b2955
unani <unani92@naver.com>
2020-08-16 17:35:58 +0900

    emaillogin loadingspanner

---
+++ frontend/src/views/LoginLoading.vue
@@ -0,0 +1,32 @@
+<template>
+  <div style="padding-top: 150px" v-if="!this.$store.getters.isLoggedIn">
+    <LoadingSpinner/>
+  </div>
+</template>
+
+<script>
+  import LoadingSpinner from "../components/common/LoadingSpinner";
+  export default {
+    name: "LoginLoading",
+    components: {
+      LoadingSpinner
+    },
+    props: {
+      redirect: {
+        type: Number && String,
+        required: false
+      }
+    },
+    updated() {
+      if (this.$store.getters.isLoggedIn) {
+        if (this.redirect) {
+          this.$router.push({ name: 'ArticleDetail', params: { id: Number(this.redirect) } })
+        } else this.$router.push('/')
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
